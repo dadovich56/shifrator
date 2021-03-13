355c5d3629147695e7eaa93b5c0498b3ac5b954c
dadovich56 <79352784+dadovich56@users.noreply.github.com>
2021-03-13 16:40:35 +0300

    Add files via upload

---
+++ shifrator.py
@@ -0,0 +1,113 @@
+alfavit = ' abcdefghijklmnopqrstuvwxyz@$-?!:1234567890абвгдеёжзийклмнопрстуфхцчшщьъыэюя'
+t = {' ': 'psihfyiftushtsuhdtdhdtr56r7tgyu45e6rt7gtf45ert6wdsdwdwdwerrrrtt',
+       'a': 'hdiejwupqpuyahjagidjvyee45t768gyuvht6dtr5t76gytf67gyu66363t66',
+       'b': 'pwpwiowkwhssfdghjkrstzwesr45ctydr76gye45t76fdr6e45t76gyde65r6',
+       'c': 'paighrgudihhfgydhgdfudtr76y8dtr76xde4rfgghvfrdfh65tgh3e3e3e3e',
+       'd': 'uegdhejgqhqoshhdudhdgddtr76y8gdtr76drserfguhjhguy78uihgvrt76u',
+       'e': 'iegeheieyuwgwguqoqigvfdtr76y8t6u89y7hwe45t76y8gdswerfdwsertyh',
+       'f': 'yiehshudydheijegdydre45t76r76te45xrs456tdre45f6d5we52wsefrre3',
+       'g': 'ueypqyggijgtuyfghsisohddtrf76y89gutf76y8u9t76y8h9guct76gyuf6t',
+       'h': 'p4ujjhahsiwswjssafaft67ytr567y3t6e7ywe765edfgu33ye783iue6738e',
+       'i': 'e9phssgxhsnvgxyseerdssar4567yt67ijhgtyuijt6u3eyw5erfg3hte53t3',
+       'j': '78nhshdhsudsbdbjkajndt567uhgt67uhgtyuyu65rfghgret6yhy33yjegey',
+       'k': '0shdbhshduw77sbtstrf76y8gut76y8guvyuij876r5e4rgder5yuhgfrtyt3',
+       'l': 'ghxdvsghhd84gbd6w792tvr567uyt567uyt5637ugefrt7uet6w7iuejhsydu',
+       'm': 'uid88vdzhhud9o89ahsij89dabakr5678ujhgfr567ujhbfewsxzaq345676j',
+       'n': '0826wbhsj72hsb27sn2b7safrtyhgfrt6yuht67uytuh5678uhbfr5678i339',
+       'o': 'osjd892gsbn72bs91bshjhdhdsjne56yhgvfdertyujhgfyujhvfghbhgvgyh',
+       'p': 'jh782hs278s1i9sb819jsnbaj92bsu34567yteasdfghjjbvce45678uygfrt',
+       'q': 'w7sb27sj2bsujsnjajskajhssnuiuhdde45678iknbgshjdiytrr56ugfvnnv',
+       'r': 'byusyudys928782ctbhr5678uyt67uyt567u3beft7uijdmnbvsftyudjhhhd',
+       's': 'iy878hw8w89ddbatyyuhd839hs8567uhgt567uh35tgdst56d7uw78disudiw',
+       't': '02jsbsgshbdguibyaibydwj8bsu82bsujbr567ytr567uyhtrt67uiuhtyuhd',
+       'u': 'h8ysjbw8sjnuiikshwkosbdfer768ygufdtre45t76er76d5678ytyggfgftg',
+       'v': '93bsyj3bhisj39snsh839sn38sou3us8sgtyhgftyuhbgtyhjhgyujhujyuhh',
+       'w': 'fjkbduw9892gbsb89suy278s9289sbyungfghgvcftyhvcfyuhgyhgftyhghj',
+       'x': 'jhbyuwui89hshsggyusgyudgyuyuw76gv627sygu78345yuhgfde4567ygfff',
+       'y': '8bhsh2u8suhahyusu2y7sajskgaisusfr567ygfr56ygfr567yfr567ytrftd',
+       'z': 'jsjw927jsh28sns7s2hsh2s72sn27s8ajsnagyi2bfuyfrtyhgfrtyhgrtygt',
+       '@': '01jhs9sjh28sf27s7hs8sjbtbgssjjahsiw828sb57ygfr56ygfftyhgftyhh',
+       '$': '09sbhdus8wuhsb27suasy8jyw7s8jwy7s28sbya8ht7t76gyudrt76yuydrui',
+       '-': '76sybuhdiyhzyjwgyuik7u2gys7sj2g7s2877s82stiagsft678ugft7r56yf',
+       '?': '28sbhbdyusajndyuy89db78wudnsdv89udjwjhdvw8r5678ugfr678ihgtfty',
+       '!': 'w88hsgusjjagswjdhsgdwdgwudhudhywui3vduzse5erftredfg87tr67uhgf',
+       ':': 'ajhdguiuiwidafyut7893dt7sijhkdwafudt7yuh3678uihdbxt56tu87ygfc',
+       '1': '28whgsuhsvy8sgyysyufgwsyhtqtyu2gys7yus76a82snsghaj4567tfgrth3',
+       '2': 'trgerr5tyddrgxdfvsdgjhbftyuhyussyuy78g7ugyfr567uhgft67ujgt67u',
+       '3': 'xtygsbhusaswydgsgydy727wt6dgb7dhgd8shgdwggdshdhwh567uhgfrt67u',
+       '4': 'gsgdwu7sgy3ss683jvsyshsayhi8wt72t78dusgyudjyfau567uhgt67y3333',
+       '5': '9sbdsghdywgdtaydi8w27hsyhsuhagygujvfhfyh2tfgys7dybsgyd567yhgf',
+       '6': '42fgsuashfayjys63fdstdts7dhshdy7gdsdyusdh567uyhgfr56yh7ui7uy7',
+       '7': '6suivgstyaisg7hsysasfasesghdhgfdhhgyy83gsuiasit678ujbgftyujh3',
+       '8': '12sdsfefeesdsdshgghhbcj72jhsghdsdjhsdj567uhgft67uhbgty3dfdfe3',
+       '9': 'xtgz77s7hsuajhxay8s8b2sviasih2usj2t678uhgftyuhbgtyuhgty3yuyuh',
+       '0': 'fdjcndjcnjdbui3yu89d38bd779su8duagu8di3guyui4567yuhgvftryuhgy',
+       'а': 'n7xn27nx7wtxhsjhbgdxjgdwd8dwufdy8iby7ijdwy7y8hi4567yujhgvftry',
+       'б': 'btysg8u2bs92kssudjw0dju2diudw9gdw8dshdsudvysyd4rgfr56hgtyhbgg',
+       'в': 'mdied8i29sh2suausjau28sh8d9hwgd8wuihdgs8uidhwg78567uhbtuh3tyj',
+       'г': 'sdu2hsiau8s2id88929idh8su9idjuwwhduwii2678ujhgftyujhgtyy3he77',
+       'д': '72su9a9s98usghidh872sidhgsud728dw0d9d0w0dis29dygsdu678uhgty67',
+       'е': 'dsid2jad92ujd2u8d9uwidbu89jtryuijjrt45676tgjh67uhgt67ygftygdd',
+       'ё': 'sd762uysdrtyu7gw7dwudgysyihd7sdw89iajd8wjihsuiduw7dw567yhgf56',
+       'ж': 'jsd280ausahhdshd8gdwuidspodiw8dgsjiodu9wgydbjkwd768uyghft56ye',
+       'з': 'xzvctysyauhsq8sy792008wsygvud67dw7ydfiahduuhw7wudu8w23345tgff',
+       'и': 'y78dsy8dhbjhwduhsjoidg78w09dbyuhsjioplwdg78wu8dsgvyd567uhgtyh',
+       'й': 'sdyusdhwujioyjhs78ujhwdy78wdh89h8h898h89g8tf7serswew768uiyt6y',
+       'к': 'sgydsdwdshdtsydh7876w5wtdg76sd564a7gdwdd7676uihgtr5678uytr6y4',
+       'л': 'w8su88sgsjsjajsdhajdaid892vsas89jhgy8htyhbhgtyhbtfyu26gd5e67w',
+       'м': 'fd7uw9ygdf6w78dbsydghnwdvg6s7dy762fgsuhvdctuhdagudyhd6gswyssh',
+       'н': '82ysyvdtysuhjdnbvctfxgyh88w7dtvbhsgvcfdtsgyuhdwby7u68gusdhsud',
+       'о': '627u8hsbgvycjiosnbhvyu89jiongu6y7wjiohd7wyvdysujhkbdvfytsyuhi',
+       'п': '8ussydgs7dwydgvtysuighdvtysdgvwtyugdyuwigdvyuwidvtyuduhwdygdd',
+       'р': 'w82jijihsdudhw8dw78ijsbydgsujidny78wudh8isdhusdhusdjisidw2r4r',
+       'с': '72usisdhsuhduhsdywudgsh8diwidisihdhiwidhshdiwdwdsiduhwjidji34',
+       'т': '28usd8sdudsudgw7dwu8dsyudgywgy7dhsuhdysuhdsgydhwhudwudgsydbfa',
+       'у': '267y78sy7sdj9wbuduh8sji9dgyudu8stgudy6y7ujihfyguhjiwbvty345rs',
+       'ф': 'w8288shajkgyeuegfdtyuhsnbgvtfgyuhsxvcrf56w7w56stzyuhrtyshisj8',
+       'х': 'uytrfde4rty25678ss67y8hgdfw67udf7wy7d56sg7usdgduwywd6wdgwydhs',
+       'ц': 't6wsswtu76wsy7682889shi8gd87wuhdgwd78wiydg7w8dwydw89dsgd78sdy',
+       'ч': 'euugshyhdnbstyduhjnsvydtgyuhj78w9jnbwt7ydisbdusvtdhivydvyaudv',
+       'ш': '0912786wftvgfc7654erdfgvhui78y65trfgvwhsuhygtcdftyauhbg7ywgw7',
+       'щ': '9ijbyikjhsduijbygu82jiuygjdnsgjdyahjdbhajkdbjadbhajdhbyw7ywgd',
+       'ь': '827uyhsgby7ujisnhbvuwg789duhbhsudyg78wvdysuhdsuyhsd78wuplkjhb',
+       'ъ': '278suhbvgyduhsnhdghjwtdyhustvdysutd7shdtybdvtywhvtyhvthjsud3d',
+       'ы': '8276tfsygvfdtrt76yhuwtf576g8duhwvtf7d8svydfgusjibhdguywhigd7q',
+       'э': 'rftgyuhbvcfx2dtr567t8yhinjbhgvcty678ydtwy9dguwf76t8dguiyudw7d',
+       'ю': '2ushs9ihsudjisbuydhujiabydhuijdbyauhdwyudhuwdjiwduyuwdjwwdhuw',
+       'я': '80bsydhwudhuduwdh7wyt5r4e3erfgvfdrtyghgdrgygvfdrftgydr5t25s4d'
+       }
+
+z = input('text: ')
+v = input('шифровать или дешифровать? ш/д: ')
+
+def shifr(z):
+	a = 0
+	b = 1
+	itog = ''
+	for i in range(len(z)):
+		itog = itog + t[str(z[a:b])]
+		a += 1
+		b += 1
+	return itog
+
+def deshifr(z):
+	a = 0
+	b = 61
+	itog = ''
+	for i in range(len(z)//61):
+		a2 = 0
+		b2 = 1
+		for i in range(len(alfavit)):
+			pp = alfavit[a2:b2]
+			if t[pp] == z[a:b]:
+				itog = itog + pp
+			a2 += 1
+			b2 += 1
+		a += 61
+		b += 61
+	return itog
+
+if v == 'ш':
+	print('итог: ' + shifr(z))
+else:
+	print('итог: ' + deshifr(z))
